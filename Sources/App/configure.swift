05d580ce72c3a7d28ae85a545e84bad6ae7e6eaa
jaebooker <jaesonbooker@gmail.com>
2019-05-07 12:44:49 -0700

    added postgres and mysql

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,12 @@
 import FluentSQLite
+import FluentMySQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
     try services.register(FluentSQLiteProvider())
-
+    try services.register(FluentMySQLProvider())
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -23,8 +24,11 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
     databases.add(database: sqlite, as: .sqlite)
+    //configure mySQL
+    let config = MySQLDatabaseConfig.root(database: "cachedb")
+    databases.add(database: MySQLDatabase(config: config), as: .mysql)
+    //register database
     services.register(databases)
-
     // Configure migrations
     var migrations = MigrationConfig()
     migrations.add(model: Cache.self, database: .sqlite)
